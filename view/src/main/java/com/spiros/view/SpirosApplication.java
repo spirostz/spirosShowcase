583aa2ea8890646db3fa497d7bb7c2dbd453e266
Spiros <tzoras.spiros@gmail.com>
2020-04-28 16:51:14 +0300

    Initial commit

---
+++ view/src/main/java/com/spiros/view/SpirosApplication.java
@@ -0,0 +1,13 @@
+package com.spiros.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirosApplication.class, args);
+	}
+
+}
---
+++ view/src/test/java/com/spiros/view/SpirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spiros.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
